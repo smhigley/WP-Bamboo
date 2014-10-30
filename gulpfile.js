a293368dbd44ebc30723ebd5a9884fd08ea41176
smhigley <sarahmhigley@gmail.com>
2014-10-30 14:34:26 -0500

    gulp, bower, basic sass/js

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    minifyCSS = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    rename = require('gulp-rename'),
+    size = require('gulp-size'),
+    pkg = require('./package.json');
+
+var paths = {
+  styles: './scss/style.scss',
+  scripts: ['./js/main.js']
+};
+
+gulp.task('styles', ['components'], function () {
+  gulp.src(paths.styles)
+    .pipe(sass({ style: 'expanded' })) // Add source maps after figuring out minify issue
+    .pipe(rename('app.css'))
+    .pipe(gulp.dest('./css/'))
+    .pipe(rename('app.min.css'))
+    .pipe(minifyCSS())
+    .pipe(size())
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('components', function() {
+  return gulp.src(['./bower_components/normalize.css/normalize.css'])
+  .pipe(rename('_normalize.scss'))
+  .pipe(gulp.dest('./bower_components/normalize.css/'));
+});
+
+gulp.task('scripts', ['lint'], function() {
+  gulp.src(paths.scripts)
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./js/'))
+    .pipe(rename('app.min.js'))
+    .pipe(uglify({ preserveComments: 'some' }))
+    .pipe(size())
+    .pipe(gulp.dest('./js/'));
+});
+
+gulp.task('lint', function () {
+  return gulp.src('./js/main.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  gulp.watch('./js/*.js', ['scripts']);
+  gulp.watch('./scss/*.scss', ['styles']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['styles', 'scripts']);
